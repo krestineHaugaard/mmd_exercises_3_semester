16f25c9203dd9695b195b1d87810899bf4f502ae
Krestine H <krha0001@stud.kea.dk>
2024-01-31 13:57:33 +0100

    generate random nr done

--- generate_random_number/grn.js
+++ generate_random_number/grn.js
@@ -1,1 +1,15 @@
-Let randomNumber = 
+// 1. Lav en funktion der console.logger et tilfældigt tal fra og med 0 til og med 100
+
+Math.floor(Math.random());
+
+// 2. Opdater din funktion så den skriver det tilfædige tal til HTML.
+
+// let randomNr = Math.floor(Math.random());
+
+// 3. Opdater din funktionen så den kan modtage parameteret “max”. Det tilfældige tal skal så være fra og med 0 til og med “max
+
+let randomNr = Math.floor(Math.random() * 101);
+
+// 4. Opdater din funktion så den også modtager en querySelector som parameter som tallet skal udskrives til
+
+document.querySelector("#number").textContent = randomNr;
---
+++ rock_paper_scissor_game/script.js
@@ -0,0 +1,13 @@
+const rockBtn = document.querySelector(".rock");
+const scissorBtn = document.querySelector(".scissors");
+const paperBtn = document.querySelector(".paper");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+
+// player 1
+
+// player 2
+
+let randomNum = Math.random() * 2;
+
+console.log(randomNum);

c20b8bfd6fb0c343ecdc036a4d744c55019457b7
Krestine H <krha0001@stud.kea.dk>
2024-02-02 09:24:47 +0100

    momsberegner update

--- moms_beregner/moms_beregner.js
+++ moms_beregner/moms_beregner.js
@@ -1,11 +1,23 @@
 "use strict";
 
-const fastMoms = 0.25;
-let valgtPris = 1000;
-momsBeregner(fastMoms, valgtPris);
+// moms trykket fra et beløb
 
-function momsBeregner(moms, pris) {
-  let momsAfPris = moms * pris;
-  let nyPris = pris - momsAfPris;
-  console.log(nyPris + " kr");
+// const fastMoms = 0.25;
+// let valgtPris = 1000;
+// momsBeregner(fastMoms, valgtPris);
+
+// function momsBeregner(moms, pris) {
+//   let momsAfPris = moms * pris;
+//   let nyPris = pris - momsAfPris;
+//   console.log(nyPris + " kr");
+// }
+
+// moms lagt i et beløv
+
+function momsBeregner(pris, moms = 25) {
+  console.log(pris + (pris * moms) / 100);
 }
+
+let beregnMoms = momsBeregner(100);
+
+console.log(beregnMoms);

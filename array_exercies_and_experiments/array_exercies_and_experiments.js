4b523c5cd075ae09ddd4ae32dde17d8900f35fa9
Krestine H <krha0001@stud.kea.dk>
2024-02-04 13:24:38 +0100

    yes

---
+++ array_exercies_and_experiments/array_exercies_and_experiments.js
@@ -0,0 +1,168 @@
+"use strict";
+
+// let letters = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`];
+
+// console.log(letters[4]);
+
+// let newarr = letters;
+// newarr[4] = `*`;
+
+// console.log(letters[4]);
+
+// Resultat: fik den første console.log til e og den anden til * ved ikke om det er korrekt
+
+// Hvad er forskellen mellem letters og newarr?
+
+// Hvordan ser hele letters array ud før og efter ændringen til newarr?
+
+// Er det muligt at ændre et array uden at ændre andre?
+
+// Hvorfor ikke?
+
+// Hvad sker der hvis du bruger const i stedet for let?
+
+// Har du nogen ide om hvorfor det er sådan?
+
+// Ny opgave
+
+// result = people.push("Draco");
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.push("Draco");
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: 4, ['Harry', 'Ron', 'Hermione', 'Draco']
+
+// result = people.pop();
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.pop();
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: Hermione, ['Harry', 'Ron']
+
+// result = people.push("Neville");
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.push("Neville");
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: 4, ['Harry', 'Ron', 'Hermione', 'Neville']
+
+// result = people.push("Luna");
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.push("Luna");
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: 4, ['Harry', 'Ron', 'Hermione', 'Luna']
+
+// result = people.slice(0,3);
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.slice(0, 3);
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: ['Harry', 'Ron', 'Hermione'], ['Harry', 'Ron', 'Hermione']
+
+// result = people.splice(1, 0, "Cho");
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.splice(1, 0, "Cho");
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: [], ['Harry', 'Cho', 'Ron', 'Hermione']
+
+// people[1] = "Ginny";
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// people[1] = "Ginny";
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: undefined, ['Harry', 'Ginny', 'Hermione']
+
+// result = people.push("Fred", "George");
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.push("Fred", "George");
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: 5, ['Harry', 'Ron', 'Hermione', 'Fred', 'George']
+
+// result = people.indexOf("Fred");
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.indexOf("Fred");
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: -1, ['Harry', 'Ron', 'Hermione']
+
+// result = people.splice(result, 1);
+
+// const people = ["Harry", "Ron", "Hermione"];
+
+// let result;
+
+// result = people.splice(result, 1);
+
+// console.log(result);
+// console.log(people);
+
+// Resultat: ['Harry'], ['Ron', 'Hermione']
+
+// Hvad gør hver metode?
+
+// Push = skubber et ny object og til ens array
+
+// Pop =
+
+// Slice =
+
+// Splice =
+
+// IndexOf =

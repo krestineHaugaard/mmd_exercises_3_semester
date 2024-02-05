67f1c03c49fb8737a3e3eb434f707a5b81a86fa0
Krestine H <krha0001@stud.kea.dk>
2024-02-05 20:12:05 +0100

    basic struktur og css done, prøvede længe men kunne simpelthen ikke finde ud af det andet

---
+++ gaet_et_tal_ombyttet/gaet_et_tal_ombyttet.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// window.addEventListener("load", startGame);
+
+const startBtn = document.querySelector("#start_btn");
+const tooHighBtn = document.querySelector("#too_high_btn");
+const tooLowBtn = document.querySelector("#too_low_btn");
+const guessedItBtn = document.querySelector("#guessed_it");
+const computerTexst = document.querySelector("#computer_tekst");

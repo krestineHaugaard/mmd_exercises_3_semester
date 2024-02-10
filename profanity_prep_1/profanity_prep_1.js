e5b32d66c25b619d6fcccb1cb8317596e7d6ca7d
Krestine H <krha0001@stud.kea.dk>
2024-02-10 01:43:03 +0100

    profanity opgaven sat op

---
+++ profanity_prep_1/profanity_prep_1.js
@@ -0,0 +1,10 @@
+let theText =
+  "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+
+// Find ordet "ord" i strengen theText og erstat den med ting
+
+// Prøv med split / join.
+
+// Prøv med replaceAll()
+
+// console.log dit resultat

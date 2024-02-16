75a0529aaa5743141a0847641b7e20416df854c8
Krestine H <krha0001@stud.kea.dk>
2024-02-16 12:48:49 +0100

    opgave sat op

--- profanity_filter/profanity_filter.js
+++ profanity_filter/profanity_filter.js
@@ -4,21 +4,7 @@ const curseWords = [
   { bad: "marquee", good: "just don't" },
 ];
 
-let mainText =
-  "Programmering er sjovt! Men husk altid at bruge var, float og marquee i moderne kodning.";
+const textElement = document.querySelector("#text");
 const fixWordsBtn = document.querySelector("#fix_text");
 const dialogPopUp = document.querySelector("#dialog_pop_up");
 const closeDialogBtn = document.querySelector("#close_dialog");
-
-fixWordsBtn.addEventListener("click", () => {
-
-  if () {
-    mainText.replace(curseWords.bad, curseWords.good);
-  } else {
-    dialogPopUp.showModal();
-  }
-});
-
-closeDialogBtn.addEventListener("click", () => {
-  dialogPopUp.close();
-});

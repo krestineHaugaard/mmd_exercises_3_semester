c30602ff30322368cc02801e765fefb9c84ccc3d
Krestine H <krha0001@stud.kea.dk>
2024-02-21 00:50:50 +0100

    updates

--- to_do_list_2/js.js
+++ to_do_list_2/js.js
@@ -1,3 +1,5 @@
+// save to local storage
+
 // tema switcher fordi det lækkert at brugeren kan "costomize" deres personlige to do liste
 
 const temaSwitch = document.querySelector(".tema");
@@ -7,24 +9,99 @@ temaSwitch.addEventListener("change", (event) => {
   document.querySelector("body").dataset.theme = nyValue;
 });
 
-// knapper i venstre side af big grid skal skifte mellem visning af brugerens to do liste og de færdige opgaver
-
-// Ny opgave knap skal åbne pop up vindue hvor bruger kan udfylde informationer
-
-// Ved pop up element skal baggrunden hvis muligt formørkes så den ikke forstyre det visuelle i pop uppen
-
-// Tilføj delelement skal gøre det muligt for bruger at putte flere delelementer på sin opgave liste
-
-// Kryds skal lukke pop up hvis bruger ikke ønsker at lave ny opgave alligevel
-
 // Tilføj opgave knap skal tilføje alt information fra pop op input boxes, til en ny article (opgave)
 
-// Lav unikt ID til hver articel der bliver oprettet, samt opret ny artikkiel for hver submit
+const addTaskBtn = document.querySelector(".grid_right_btn");
+
+const TaskInfo = {
+  name: "undefined",
+  number: 0,
+  id: "unknown",
+  checkbox: "null",
+};
+
+const allTask = [];
+
+addTaskBtn.addEventListener("click", () => {
+  const newTask = Object.create(TaskInfo);
+
+  const taskName = document.querySelector("#text").value;
+  const taskNumber = document.querySelector("#number").value;
+  const taskID = unikIDCode();
+  newTask.name = taskName;
+  newTask.number = taskNumber;
+  newTask.id = taskID;
+
+  allTask.push(newTask);
+  document.querySelector("form").reset();
+  displayTask();
+});
+
+function displayTask() {
+  document.querySelector("#task_list").innerHTML = "";
+  allTask.forEach(createTaskList);
+}
+
+function createTaskList(newTask) {
+  const clone = document.querySelector("#template").content.cloneNode(true);
+
+  clone.querySelector("article").id = newTask.id;
+  clone.querySelector("[data-type=name]").textContent = newTask.name;
+  clone.querySelector("[data-type=number]").textContent = newTask.number;
+  const checkBox = clone.querySelector("[type=checkbox]");
+  checkBox.addEventListener("change", () => {
+    checkBoxValue(newTask.id);
+  });
+  const deleteBtn = clone.querySelector(".exit");
+  deleteBtn.addEventListener("click", () => {
+    deleteTask(newTask.id);
+  });
+  document.querySelector("#task_list").appendChild(clone);
+}
+
+// Lav unikt ID til hver list item der bliver oprettet, samt opret ny artikkiel for hver submit
+
+function unikIDCode() {
+  let unikID = self.crypto.randomUUID();
+  return unikID;
+}
 
 // Store tjek tegns box skal fjerne opgaven fra to do til færdige opgaver og omvendt
 
+function checkBoxValue(taskId) {
+  const task = allTask.find((task) => task.id === taskId);
+  return task ? task.checkbox : null;
+}
+
 // Stor skraldespands icon skal fjerne opgaver permanent
 
-// Lille tjek tegn skal bare krydse delelementer af som gjort
+function deleteTask(taskId) {
+  const taskIndex = allTask.findIndex((task) => task.id === taskId);
 
-// Lille skraldespand skal fjerne fjerne enkelte delelment i den samlede opgave
+  if (taskIndex !== -1) {
+    allTask.splice(taskIndex, 1);
+    displayTask();
+  }
+}
+
+// knapper i venstre side af big grid skal skifte mellem visning af brugerens to do liste og de færdige opgaver
+
+const tdListBtn = document.querySelector("#to_do_list_btn");
+const fListBtn = document.querySelector("#finished_list_btn");
+const tdList = document.querySelector("#to_do_list");
+const fList = document.querySelector("#finished_task_list");
+tdListBtn.style.backgroundColor = "var(--background-col)";
+
+tdListBtn.addEventListener("click", () => {
+  fListBtn.style.backgroundColor = "var(--btn-col)";
+  tdListBtn.style.backgroundColor = "var(--background-col)";
+  fList.classList.add("hide");
+  tdList.classList.remove("hide");
+});
+
+fListBtn.addEventListener("click", () => {
+  fListBtn.style.backgroundColor = "var(--background-col)";
+  tdListBtn.style.backgroundColor = "var(--btn-col)";
+  tdList.classList.add("hide");
+  fList.classList.remove("hide");
+});
